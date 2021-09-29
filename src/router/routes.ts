c6153ea12b71453b368d05add5af42838ea12618
Tassilo Dietze <t.dietze@itdotmedia.de>
2021-09-29 14:45:20 +0200

    Added router

---
+++ src/router/routes.ts
@@ -0,0 +1,10 @@
+import Home from '@/views/Home.vue';
+
+/** @type {import('vue-router').RouterOptions['routes']} */
+export const routes = [
+  {
+    name: 'Home',
+    path: '/',
+    component: Home,
+  },
+];
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import path from 'path';
+
+// const productionPath = '/';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // base: process.env.NODE_ENV == 'production' ? productionPath : '/',
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 });
