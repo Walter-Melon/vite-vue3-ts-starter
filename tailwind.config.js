95f828e765470c56223cb38112f4c3b4e3139174
Tassilo Dietze <t.dietze@itdotmedia.de>
2021-09-29 15:04:33 +0200

    Added tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+const colors = require('tailwindcss/colors');
+
+/** @type {import("@types/tailwindcss/tailwind-config").TailwindConfig } */
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: colors.indigo,
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
+};
