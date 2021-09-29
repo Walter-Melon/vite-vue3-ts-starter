a6d70776e07ab266a79e9fd87087daa46f38ece6
Walter Mc Melon <mc.walter.melon@gmail.com>
2021-09-29 18:04:14 +0200

    Change to default dark Added themed navigation Removed example component

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,15 @@
 <template>
-  <img class="m-auto" alt="Vue logo" src="./assets/logo.png" />
-  <router-view />
+  <div class="h-screen">
+    <TheNavigation />
+    <main>
+      <router-view />
+    </main>
+  </div>
 </template>
+
+<script setup lang="ts">
+import TheNavigation from '@/layouts/TheNavigation.vue';
+
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,24 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from '@/components/HelloWorld.vue';
-</script>
-
 <template>
-  <HelloWorld msg="Hello, World." />
+  <div class="py-6">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+      <h1 class="text-2xl font-semibold text-primary-600">
+        {{ title }}
+      </h1>
+    </div>
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+      <!-- Replace with your content -->
+      <div class="py-4">
+        <div
+          class="border-2 border-dashed border-gray-200 rounded-lg h-96"
+        ></div>
+      </div>
+      <!-- /End replace -->
+    </div>
+  </div>
 </template>
+
+<script setup lang="ts">
+import { useTitle } from '@vueuse/core';
+
+const title = useTitle('Home');
+</script>
